fe0f1d5a1ad5260d630cb06971543a9288f1efdc
sora_uzu <naruto07170717@gmail.com>
2024-12-29 15:07:44 +0900

    add button

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 5, "Medium":7, "Hard": 12]
+    @IBAction func HardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let result = eggTimes[hardness]!
+        print(result)
+    }
     
-
-
 }
