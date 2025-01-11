fa6c2b7c4ee029715752ec3893d83c7fe0fdd89c
sora_uzu <naruto07170717@gmail.com>
2025-01-11 10:22:01 +0900

    done

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -22,17 +22,21 @@ class ViewController: UIViewController {
         
         timer.invalidate()
         let hardness = sender.currentTitle!
-        
         totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
         if secondsPassed < totalTime {
+            secondsPassed += 1
             let percentageProgress = Float(secondsPassed) / Float(totalTime)
             progressBar.progress = percentageProgress
-            secondsPassed += 1
+          
         } else {
             timer.invalidate()
             titleLabel.text = "Done!"
